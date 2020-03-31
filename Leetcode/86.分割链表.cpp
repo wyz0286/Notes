9a5f53c81ef8d93ce356404ea3a2a4160d2e1e7c
wyz0286 <1204320959@qq.com>
2020-03-31 11:41:15 +0800

    首次一遍通过链表题/考虑优化/添加头结点方便处理(中等)

---
+++ Leetcode/86.分割链表.cpp
@@ -0,0 +1,60 @@
+﻿//86.分隔链表
+//给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前
+//
+//你应当保留两个分区中每个节点的初始相对位置
+//
+//示例 :
+//
+//输入: head = 1->4->3->2->5->2, x = 3
+//输出 : 1->2->2->4->3->5
+
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {} 
+};
+ 
+ListNode* partition(ListNode* head, int x) {
+    ListNode* dummy1 = new ListNode(NULL);
+    ListNode* p = dummy1;
+
+    ListNode* dummy2 = new ListNode(NULL);
+    ListNode* q = dummy2;
+
+    ListNode* r = head;
+    while (r)
+    {
+        if (r->val < x)
+        {
+            ListNode* tmp = new ListNode(r->val);
+            p->next = tmp;
+            p = tmp;
+        }
+        else
+        {
+            ListNode* tmp = new ListNode(r->val);
+            q->next = tmp;
+            q = tmp;
+        }
+        r = r->next;
+    }
+    p->next = dummy2->next;
+    delete(dummy2);
+    return dummy1->next;
+}
+
+int main()
+{
+    vector<int> a{ 1, 1, 2, 4, 4, 4, 4, 4 };
+    vector<int> b{ 1, 1, 1, 1, 1, 2, 3, 3 };
+    /*cout << majorityElement(a)<< endl;
+    cout << majorityElement(b)<< endl;*/
+    return 0;
+}
