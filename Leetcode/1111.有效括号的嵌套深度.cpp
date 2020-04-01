8ce157f02fa8ae40db68c993bb530da87f464cd2
wyz0286 <1204320959@qq.com>
2020-04-01 09:57:29 +0800

    题目晦涩难懂/用一个变量模拟栈的深度(中等)

---
+++ Leetcode/1111.有效括号的嵌套深度.cpp
@@ -0,0 +1,34 @@
+﻿//1111.有效括号的嵌套深度
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+vector<int> maxDepthAfterSplit(string seq) {
+    int d = 0;
+    vector<int> ans;
+    for (char& c : seq)
+        if (c == '(') {
+            ++d;
+            ans.push_back(d % 2);
+        }
+        else {
+            ans.push_back(d % 2);
+            --d;
+        }
+    return ans;
+}
+
+int main()
+{
+    vector<int> a = maxDepthAfterSplit("()(())()");
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
