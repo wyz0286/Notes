280ed646467e4cd934fa0bb3251a93cb8e4891be
wyz0286 <1204320959@qq.com>
2020-04-04 09:25:12 +0800

    使用双指针(困难)

---
+++ Leetcode/42.接雨水.cpp
@@ -0,0 +1,32 @@
+﻿//42.接雨水
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int trap(vector<int>& height)
+{
+    int left = 0, right = height.size() - 1;
+    int ans = 0;
+    int left_max = 0, right_max = 0;
+    while (left < right) {
+        if (height[left] < height[right]) {
+            height[left] >= left_max ? (left_max = height[left]) : ans += (left_max - height[left]);
+            ++left;
+        }
+        else {
+            height[right] >= right_max ? (right_max = height[right]) : ans += (right_max - height[right]);
+            --right;
+        }
+    }
+    return ans;
+}
+
+
+int main()
+{
+    vector<int> a = { 0,1,0,2,1,0,1,3,2,1,2,1 };
+    cout << trap(a) << endl;
+    return 0;
+}
