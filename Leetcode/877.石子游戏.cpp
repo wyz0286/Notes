357b5778cb0c201a6e280ab28d653712fb06556a
wyz0286 <1204320959@qq.com>
2020-04-06 11:11:26 +0800

    智力题数学方法/遇事不决动态规划(中等)

---
+++ Leetcode/877.石子游戏.cpp
@@ -0,0 +1,32 @@
+﻿//877.石子游戏
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool stoneGame(vector<int>& piles) {
+    int n = piles.size();
+
+    vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+
+    for (int size = 1; size <= n; ++size)
+        for (int i = 1, j = size; j <= n; ++i, ++j) 
+        {
+            int parity = (j + i + n) % 2;  // j - i - N; but +x = -x (mod 2)
+            if (parity == 1)
+                dp[i][j] = max(piles[i - 1] + dp[i + 1][j], piles[j - 1] + dp[i][j - 1]);
+            else
+                dp[i][j] = min(-piles[i - 1] + dp[i + 1][j], -piles[j - 1] + dp[i][j - 1]);
+        }
+
+    return dp[1][n] > 0;
+
+}
+
+int main()
+{
+    vector<int> a = { 5,3,4,5 };
+    cout << stoneGame(a) << endl;
+}
