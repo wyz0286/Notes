16680b9d910b4ee05a696caddba61fc81c8610b4
wyz0286 <1204320959@qq.com>
2020-04-10 09:38:10 +0800

    C++ stringstream/自定义deal函数(中等)

---
+++ Leetcode/151.翻转字符串里的单词.cpp
@@ -0,0 +1,80 @@
+﻿//151.翻转字符串里的单词
+
+//给定一个字符串，逐个翻转字符串中的每个单词。
+
+//示例 1：
+//输入 : "the sky is blue"
+//输出 : "blue is sky the"
+//
+//示例 2：
+//输入 : "  hello world!  "
+//输出 : "world! hello"
+//解释 : 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括
+//
+//示例 3：
+//输入 : "a good   example"
+//输出 : "example good a"
+//解释 : 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个
+//
+//说明：
+//无空格字符构成一个单词。
+//输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括
+//如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个
+
+
+#include <iostream>
+#include <random>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <random>
+#include <sstream>
+
+using namespace std;
+
+//string reverseWords(string s) {
+//    stringstream ss(s);
+//    string str, ans;
+//    int flag = 0;
+//    while (ss >> str)
+//    {
+//        if(!flag)
+//            ans = str;
+//        else 
+//            ans = str + " " + ans;
+//        flag = 1;
+//    } 
+//    return ans;
+//}
+
+string reverseWords(string s) {
+    // 反转整个字符串
+    reverse(s.begin(), s.end());
+
+    int n = s.size();
+    int idx = 0;
+    for (int start = 0; start < n; ++start) {
+        if (s[start] != ' ') {
+            // 填一个空白字符然后将idx移动到下一个单词的开头位置
+            if (idx != 0) s[idx++] = ' ';
+
+            // 循环遍历至单词的末尾
+            int end = start;
+            while (end < n && s[end] != ' ') s[idx++] = s[end++];
+
+            // 反转整个单词
+            reverse(s.begin() + idx - (end - start), s.begin() + idx);
+
+            // 更新start，去找下一个单词
+            start = end;
+        }
+    }
+    s.erase(s.begin() + idx, s.end());
+    return s;
+}
+
+int main()
+{
+    cout << reverseWords("the sky is blue") << endl;
+    return 0;
+}
