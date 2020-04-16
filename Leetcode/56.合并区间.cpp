3ef88991191a29f8141bb1ef277976e17679c275
wyz0286 <1204320959@qq.com>
2020-04-16 09:22:56 +0800

    先排序再合并(中等)

---
+++ Leetcode/56.合并区间.cpp
@@ -0,0 +1,50 @@
+﻿//56.合并区间
+
+//给出一个区间的集合，请合并所有重叠的区间
+//
+//示例 1:
+//
+//输入: [[1, 3], [2, 6], [8, 10], [15, 18]]
+//输出 : [[1, 6], [8, 10], [15, 18]]
+//解释 : 区间[1, 3] 和[2, 6] 重叠, 将它们合并为[1, 6]
+//
+//示例 2 :
+//
+//输入 : [[1, 4], [4, 5]]
+//输出 : [[1, 5]]
+//解释 : 区间[1, 4] 和[4, 5] 可被视为重叠区间
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <random>
+
+using namespace std;
+
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    if (intervals.size() == 0) 
+    {
+        return {};
+    }
+    sort(intervals.begin(), intervals.end());
+    vector<vector<int>> merged;
+    for (int i = 0; i < intervals.size(); ++i) 
+    {
+        int L = intervals[i][0], R = intervals[i][1];
+        if (!merged.size() || merged.back()[1] < L) 
+        {
+            merged.push_back({ L, R });
+        }
+        else 
+        {
+            merged.back()[1] = max(merged.back()[1], R);
+        }
+    }
+    return merged;
+}
+
+int main()
+{
+	return 0;
+}
