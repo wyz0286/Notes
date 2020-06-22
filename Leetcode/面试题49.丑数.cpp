44d1ea9158fc8b0827685d07d9b93dbc56a80824
wyz0286 <1204320959@qq.com>
2020-06-22 10:08:23 +0800

    必看系列(中等)

---
+++ Leetcode/面试题49.丑数.cpp
@@ -0,0 +1,40 @@
+﻿//面试题49.丑数
+
+//我们把只包含因子 2、3 和 5 的数称作丑数（Ugly Number）
+//
+//求按从小到大的顺序的第 n 个丑数
+//
+//示例:
+//
+//输入: n = 10
+//输出 : 12
+//解释 : 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 是前 10 个丑数
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int nthUglyNumber(int n) {
+	vector<int> dp(n, 0);
+	dp[0] = 1;
+	int p2 = 0, p3 = 0, p5 = 0;
+	for (int i = 1; i < n; i++) 
+	{
+		dp[i] = min(min(dp[p2] * 2, dp[p3] * 3), dp[p5] * 5);
+		if (dp[i] == dp[p2] * 2)
+			p2++;
+		if (dp[i] == dp[p3] * 3)
+			p3++;
+		if (dp[i] == dp[p5] * 5)
+			p5++;
+	}
+	return dp[n - 1];
+}
+
+int main()
+{ 
+	cout << nthUglyNumber(20) << endl;
+    return 0;
+}
