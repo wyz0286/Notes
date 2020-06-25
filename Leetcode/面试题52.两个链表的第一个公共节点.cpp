a815e900d92357dd8444732ed073e9da6ccd45a6
wyz0286 <1204320959@qq.com>
2020-06-25 09:50:03 +0800

    都分别遍历A+B相遇即公共节点(简答)

---
+++ Leetcode/面试题52.两个链表的第一个公共节点.cpp
@@ -0,0 +1,33 @@
+﻿//面试题52.两个链表的第一个公共节点
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+ 
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        ListNode* node1 = headA;
+        ListNode* node2 = headB;
+
+        while (node1 != node2) {
+            node1 = node1 != NULL ? node1->next : headB;
+            node2 = node2 != NULL ? node2->next : headA;
+        }
+        return node1;
+
+    }
+};
+
+int main()
+{ 
+    return 0;
+}
